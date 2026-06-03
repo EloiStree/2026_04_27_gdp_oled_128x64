4fd9e49f6b61d37b4b5768a9961c9afdcf68d83a
Eloi Stree <eloistree@gmail.com>
2026-06-03 10:14:42 +0200

    Save

---
+++ feature/draw_with_sphere/ssd_.1306_bridge_ui_buttons_to_facade.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@export var button_clear:Button
+@export var button_full:Button
+@export var button_inverse_display_color:Button
+@export var button_inverse_array_value:Button
+
+#@export_group("Style") 
+#@expor var button_
